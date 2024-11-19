b9e44e3fcba2933dc484843f2750bf95a5e4245d
Enes <60015283+enesop@users.noreply.github.com>
2024-11-19 01:59:56 +0000

    Add emit command

---
+++ commands/emit.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+// Handle command
+exports.run = async (client, interaction) => {
+
+  await client.emit('guildMemberAdd', interaction.user);
+  
+  interaction.reply({
+    embeds: [embed],
+    ephemeral: false,
+  });
+};
+
+// Register command
+const cmd = new Discord.SlashCommandBuilder()
+  .setName('emit')
+  .setDescription('Emit an event')
+  .setDMPermission(false)
+  .setDefaultMemberPermissions(8)
+  .addSubcommand(
+    new Discord.SlashCommandSubcommandBuilder()
+      .setName('guildMemberAdd')
+      .setDescription("Emit a guild member added event")
+  );
+
+exports.data = cmd.toJSON();
